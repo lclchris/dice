79d8432d012cc652d0038ad3692c28f32c2c544a
lclchris <49675446+lclchris@users.noreply.github.com>
2021-09-11 05:47:33 +0700

    add roll & reset button

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ function random() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-var randomNumber1 = random();
-var randomNumber2 = random();
+function rollDice() {
+  var randomNumber1 = random();
+  var randomNumber2 = random();
 
-var randomDiceImg1 = document
-  .querySelector(".img1")
-  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
-var randomDiceImg2 = document
-  .querySelector(".img2")
-  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector(".button2").removeAttribute("disabled");
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector(".button2").removeAttribute("disabled");
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector(".button2").removeAttribute("disabled");
+  }
+}
+
+function reset() {
+  return document.location.reload();
 }
